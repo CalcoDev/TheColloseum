c51f7dcc1943c23548f1e1a1011e8810a8511fa7
Calco Dev <calcopoddev@gmail.com>
2023-09-08 13:47:02 +0300

    Refactored ase_context.h to remove redeclared macro warnings.

--- src/base/base_context.h
+++ src/base/base_context.h
@@ -1,17 +1,6 @@
 #ifndef BASE_CONTEXT_H
 #define BASE_CONTEXT_H
 
-// NOTE(calco): Defaults
-// NOTE(calco): -- Compilers --
-#define BASE_COMPILER_GCC   0
-#define BASE_COMPILER_MSVC  0
-#define BASE_COMPILER_CLANG 0
-
-// NOTE(calco): -- Operating Systems --
-#define BASE_OS_WIN   0
-#define BASE_OS_LINUX 0
-#define BASE_OS_MAC   0
-
 // NOTE(calco): Actual Data (aka a mess)
 #ifdef _MSC_VER
 #  define BASE_COMPILER_MSVC 1
@@ -33,6 +22,29 @@
 #  define BASE_OS_MAC 1
 #elif __linux__
 #  define BASE_OS_LINUX 1
+#endif
+
+// NOTE(calco): Defaults
+// NOTE(calco): -- Compilers --
+#ifndef BASE_COMPILER_GCC
+#  define BASE_COMPILER_GCC 0
+#endif
+#ifndef BASE_COMPILER_MSVC
+#  define BASE_COMPILER_MSVC 0
+#endif
+#ifndef BASE_COMPILER_CLANG
+#  define BASE_COMPILER_CLANG 0
+#endif
+
+// NOTE(calco): -- Operating Systems --
+#ifndef BASE_OS_WIN
+#  define BASE_OS_WIN 0
+#endif
+#ifndef BASE_OS_LINUX
+#  define BASE_OS_LINUX 0
+#endif
+#ifndef BASE_OS_MAC
+#  define BASE_OS_MAC 0
 #endif
 
 #endif
