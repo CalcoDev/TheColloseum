8fe5b64319ec9465e4305aabfff0eddf576bb5da
= <calcopoddev@gmail.com>
2023-09-23 18:27:04 +0300

    Basic setup for input handling and custom drawing.

---
+++ src/input/input.h
@@ -0,0 +1,17 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+// TODO(calco): This is XNA style input. I have no clue whether this is good
+// or not.
+
+#include "base/base_bitset.h"
+
+typedef struct I_KeyboardState
+{
+}
+
+typedef struct I_InputState
+{
+}
+
+#endif
